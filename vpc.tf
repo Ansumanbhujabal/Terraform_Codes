516b6573eaaa043e2dbe1cb7268f0acc48ece4a4
Ansuman Simanta Sekhar Bhujabala <106860608+Ansumanbhujabal@users.noreply.github.com>
2024-05-12 18:02:55 +0530

    public subnet add

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,15 @@ resource "aws_vpc" "test" {
     Name = "test"
   }
 
+# PUBLIC SUBNET 
+resource "aws_subnet" "test-web" {
+  vpc_id     = aws_vpc.test.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone ="ap-northeast-1"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "test-web-subnet"
+  }
+}
+
